4d91ba44c8df47dd5699b63d314e7be1234635cd
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-02-12 16:02:21 +0100

    emoji: Make the emoji class public

--- emojis/emojis.gradle.kts
+++ emojis/emojis.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.2"
+version = "0.0.3"
 
 project.extra["PluginName"] = "Emojis"
 project.extra["PluginDescription"] = "Replaces common emoticons such as :) with their corresponding emoji in the chat"
