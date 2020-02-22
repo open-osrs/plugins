a69a8b8012fe31ad32c38c47bdc5da5af53ab07d
Kyle <48519776+xKylee@users.noreply.github.com>
2020-02-22 15:23:06 +0000

    statusorbs: fix config issue (#68)
    * Update statusorbs.gradle.kts
    
    * Update StatusOrbsConfig.java
    
    * Update StatusOrbsPlugin.java
    
    * Update statusorbs.gradle.kts
    
    * Update statusorbs.gradle.kts

--- statusorbs/statusorbs.gradle.kts
+++ statusorbs/statusorbs.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.4"
+version = "0.0.5"
 
 project.extra["PluginName"] = "Status Orbs"
 project.extra["PluginDescription"] = "Configure settings for the Minimap orbs"
