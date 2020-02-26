4feebb2c805c98a57e0bfd5623a6418eeeddacc4
Kyle <48519776+xKylee@users.noreply.github.com>
2020-02-26 16:47:58 +0000

    Update MenuEntrySwapperPlugin.java

--- menuentryswapper/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ menuentryswapper/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -35,7 +35,6 @@ import com.google.inject.Provides;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
