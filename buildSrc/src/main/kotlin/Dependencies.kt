d35675b53a97dfdb5d5151d6e0b3795ea9ccc5e9
Ben Riggs <42555892+ben93riggs@users.noreply.github.com>
2020-03-11 17:50:34 -0500

    Update Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,37 +24,34 @@
  */
 
 object ProjectVersions {
-    const val rlVersion = "1.6.8"
+    const val rlVersion = "3.1.0"
     const val apiVersion = "0.0.1"
 }
 
 object Libraries {
     private object Versions {
-        const val annotations = "18.0.0"
-        const val apacheCommonsLang = "3.9"
+        const val annotations = "19.0.0"
         const val apacheCommonsText = "1.8"
         const val discord = "1.1"
         const val gson = "2.8.6"
         const val guava = "28.2-jre"
         const val guice = "4.2.2"
         const val hamcrest = "2.2"
-        const val javax = "1.3.2"
         const val jogamp = "2.3.2"
         const val jopt = "5.0.4"
-        const val jooq = "3.12.3"
+        const val jooq = "3.13.0"
         const val junit = "4.13"
         const val logback = "1.2.3"
-        const val lombok = "1.18.10"
+        const val lombok = "1.18.12"
         const val mockito = "3.2.4"
-        const val okhttp3 = "4.3.0"
+        const val okhttp3 = "4.4.0"
         const val pf4j = "3.2.0"
         const val radiance = "2.5.1"
-        const val rxjava = "2.2.16"
+        const val rxjava = "3.0.0"
         const val slf4j = "1.7.30"
     }
 
     const val annotations = "org.jetbrains:annotations:${Versions.annotations}"
-    const val apacheCommonsLang = "org.apache.commons:commons-lang3:${Versions.apacheCommonsLang}"
     const val apacheCommonsText = "org.apache.commons:commons-text:${Versions.apacheCommonsText}"
     const val discord = "net.runelite:discord:${Versions.discord}"
     const val gson = "com.google.code.gson:gson:${Versions.gson}"
@@ -62,7 +59,6 @@ object Libraries {
     const val guice = "com.google.inject:guice:${Versions.guice}:no_aop"
     const val guiceTestlib = "com.google.inject.extensions:guice-testlib:${Versions.guice}"
     const val hamcrest = "org.hamcrest:hamcrest-library:${Versions.hamcrest}"
-    const val javax = "javax.annotation:javax.annotation-api:${Versions.javax}"
     const val jogampJogl = "org.jogamp.jogl:jogl-all:${Versions.jogamp}"
     const val jogampGluegen = "org.jogamp.gluegen:gluegen-rt:${Versions.jogamp}"
     const val jopt = "net.sf.jopt-simple:jopt-simple:${Versions.jopt}"
@@ -74,7 +70,7 @@ object Libraries {
     const val mockitoInline = "org.mockito:mockito-inline:${Versions.mockito}"
     const val okhttp3 = "com.squareup.okhttp3:okhttp:${Versions.okhttp3}"
     const val pf4j = "org.pf4j:pf4j:${Versions.pf4j}"
-    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxjava = "io.reactivex.rxjava3:rxjava:${Versions.rxjava}"
     const val slf4jApi = "org.slf4j:slf4j-api:${Versions.slf4j}"
     const val substance = "org.pushing-pixels:radiance-substance:${Versions.radiance}"
 
