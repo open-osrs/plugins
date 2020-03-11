09909c593b835a263ec23320958ee46644f661c6
Ben Riggs <42555892+ben93riggs@users.noreply.github.com>
2020-03-11 18:22:26 -0500

    updated versions

--- itemdropper/itemdropper.gradle.kts
+++ itemdropper/itemdropper.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.4"
+version = "0.0.5"
 
 project.extra["PluginName"] = "Item Dropper"
 project.extra["PluginDescription"] = "Automatically drop all specified items"
