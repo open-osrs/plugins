5e210fba00460d07ec0a72339db566419e140d81
Kyle <48519776+xKylee@users.noreply.github.com>
2020-03-28 22:49:41 +0000

    herbi: add plugintype (#151)
    * Update herbiboar.gradle.kts
    
    * Update HerbiboarPlugin.java

--- herbiboar/herbiboar.gradle.kts
+++ herbiboar/herbiboar.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.5"
+version = "0.0.6"
 
 project.extra["PluginName"] = "Herbiboar"
 project.extra["PluginDescription"] = "Highlight starting rocks, trails, and the objects to search at the end of each trail"
