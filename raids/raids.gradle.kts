839a8fd1a90e4a096c85e371daf51825a4d513b1
Crystalknoct <andrewj0013@gmail.com>
2020-03-31 14:04:10 -0600

    raids:aids
    check style

--- raids/raids.gradle.kts
+++ raids/raids.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.4"
+version = "0.0.7"
 
 project.extra["PluginName"] = "CoX Scouter"
 project.extra["PluginDescription"] = "Show helpful information for the Chambers of Xeric raid"
