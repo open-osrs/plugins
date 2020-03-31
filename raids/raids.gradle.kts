7c6791e87c507ee50813d6054b61a32dbb9c6216
Crystalknoct <andrewj0013@gmail.com>
2020-03-31 16:22:47 -0600

    raids:aids
    Updated raids recommendation items added the following are all in syntax some were already. [Muttadiles, zamorak godsword, ice barrage],[Vasa,zamorakian hasta, abyssal dagger, Blade of saeldor, ghrazi rapier, vengeance],[Guardians,Dragon pickaxe],[Mystics,Salve amulet(ei)],[Shamans,Sanfew serum(4), Antidote++(4)],[Vespula,Prayer potion(4), Super restore(4), Sanfew Serum(4)],[Tekton,Elder Maul, Abyssal Bludgeon, Vengeance],[Thieving,lockpick],[Ice Demon, fire surge]

--- raids/raids.gradle.kts
+++ raids/raids.gradle.kts
@@ -24,7 +24,6 @@ import ProjectVersions.rlVersion
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 version = "0.0.7"
 
 project.extra["PluginName"] = "CoX Scouter"
