0e139e4783172451490413c3c291316cf0e753b8
Crystalknoct <andrewj0013@gmail.com>
2020-03-31 16:25:11 -0600

    raids:aids
    Updated raids recommendation items added the following are all in syntax some were already. [Muttadiles, zamorak godsword, ice barrage],[Vasa,zamorakian hasta, abyssal dagger, Blade of saeldor, ghrazi rapier, vengeance],[Guardians,Dragon pickaxe],[Mystics,Salve amulet(ei)],[Shamans,Sanfew serum(4), Antidote++(4)],[Vespula,Prayer potion(4), Super restore(4), Sanfew Serum(4)],[Tekton,Elder Maul, Abyssal Bludgeon, Vengeance],[Thieving,lockpick],[Ice Demon, fire surge]

--- raids/src/main/java/net/runelite/client/plugins/raids/RaidsPlugin.java
+++ raids/src/main/java/net/runelite/client/plugins/raids/RaidsPlugin.java
@@ -24,6 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 package net.runelite.client.plugins.raids;
 
 import com.google.common.base.Joiner;
