b1bfdfb824062dcbcfbf0b83ca910fe2735ba85a
stone-wall <stonewall@pklite.xyz>
2020-04-16 11:31:12 -0400

    add config for feature update

--- screenshot/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotPlugin.java
+++ screenshot/src/main/java/net/runelite/client/plugins/screenshot/ScreenshotPlugin.java
@@ -85,6 +85,7 @@ import net.runelite.client.ui.ClientUI;
 import net.runelite.client.ui.DrawManager;
 import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.ui.overlay.OverlayRenderer;
 import net.runelite.client.util.HotkeyListener;
 import net.runelite.client.util.ImageCapture;
 import net.runelite.client.util.ImageUtil;
@@ -157,6 +158,9 @@ public class ScreenshotPlugin extends Plugin
 
 	@Inject
 	private DrawManager drawManager;
+
+	@Inject
+	private OverlayRenderer overlayRenderer;
 
 	@Inject
 	private ScheduledExecutorService executor;
@@ -577,12 +581,17 @@ public class ScreenshotPlugin extends Plugin
 	 */
 	private void takeScreenshot(String fileName)
 	{
+		if (!config.includeOverlays())
+		{
+			overlayRenderer.setShouldRender(false);
+		}
 		if (client.getGameState() == GameState.LOGIN_SCREEN)
 		{
 			// Prevent the screenshot from being captured
 			log.info("Login screenshot prevented");
 			return;
 		}
+
 
 		Consumer<Image> imageCallback = (img) ->
 		{
@@ -615,6 +624,10 @@ public class ScreenshotPlugin extends Plugin
 			log.info("Login screenshot prevented");
 			return;
 		}
+		if (!config.includeOverlays())
+		{
+			overlayRenderer.setShouldRender(false);
+		}
 
 		Consumer<Image> imageCallback = (img) ->
 		{
@@ -635,6 +648,10 @@ public class ScreenshotPlugin extends Plugin
 
 	private void takeScreenshot(String fileName, String subDir, Image image)
 	{
+		if (!config.includeOverlays())
+		{
+			overlayRenderer.setShouldRender(false);
+		}
 		BufferedImage screenshot = config.includeFrame()
 			? new BufferedImage(clientUi.getWidth(), clientUi.getHeight(), BufferedImage.TYPE_INT_ARGB)
 			: new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
@@ -665,6 +682,7 @@ public class ScreenshotPlugin extends Plugin
 		// Draw the game onto the screenshot
 		graphics.drawImage(image, gameOffsetX, gameOffsetY, null);
 		imageCapture.takeScreenshot(screenshot, fileName, subDir, config.notifyWhenTaken(), config.uploadScreenshot());
+		overlayRenderer.setShouldRender(true);
 	}
 
 	@VisibleForTesting
