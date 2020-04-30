83e41ced41c0833cce804a20b6dbb1f481c3bf1e
Kyle <48519776+xKylee@users.noreply.github.com>
2020-05-01 00:30:03 +0100

    antidrag: various fixes (#212)
    * Update antidrag.gradle.kts
    
    * Update AntiDragConfig.java
    
    * Update AntiDragPlugin.java

--- antidrag/antidrag.gradle.kts
+++ antidrag/antidrag.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.7"
+version = "0.0.8"
 
 project.extra["PluginName"] = "Anti Drag"
 project.extra["PluginDescription"] = "Prevent dragging an item for a specified delay"
