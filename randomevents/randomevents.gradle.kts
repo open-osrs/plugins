ff5d903f060694ac41e77a3ec9c1a94a5a0bd5ca
Kyle <48519776+xKylee@users.noreply.github.com>
2020-04-30 12:25:37 +0100

    randomevents

--- randomevents/randomevents.gradle.kts
+++ randomevents/randomevents.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.3"
+version = "0.0.4"
 
 project.extra["PluginName"] = "Random Events"
 project.extra["PluginDescription"] = "Notify when random events appear and remove talk/dismiss options on events that aren't yours"
