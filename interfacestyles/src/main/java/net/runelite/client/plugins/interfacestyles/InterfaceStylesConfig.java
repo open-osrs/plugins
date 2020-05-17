2d6f8c2d077643670da0f480b74e716e49b479cc
Kyle <48519776+xKylee@users.noreply.github.com>
2020-05-17 01:35:24 +0100

    Update InterfaceStylesConfig.java

--- interfacestyles/src/main/java/net/runelite/client/plugins/interfacestyles/InterfaceStylesConfig.java
+++ interfacestyles/src/main/java/net/runelite/client/plugins/interfacestyles/InterfaceStylesConfig.java
@@ -29,6 +29,7 @@ package net.runelite.client.plugins.interfacestyles;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
 @ConfigGroup("interfaceStyles")
 public interface InterfaceStylesConfig extends Config
