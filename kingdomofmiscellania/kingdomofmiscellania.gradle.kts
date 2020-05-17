a862da478b75b14b50a705302d790b9f1594bd02
Kyle <48519776+xKylee@users.noreply.github.com>
2020-05-17 01:44:41 +0100

    Update kingdomofmiscellania.gradle.kts

--- kingdomofmiscellania/kingdomofmiscellania.gradle.kts
+++ kingdomofmiscellania/kingdomofmiscellania.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.5"
+version = "0.0.6"
 
 project.extra["PluginName"] = "Kingdom of Miscellania"
 project.extra["PluginDescription"] = "Show various informations about your Kingdom of Miscellania"
