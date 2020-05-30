9d90896982417348ba084a0c6e491aa0be059da6
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-05-30 10:27:22 +0200

    npchighlight: Implement isInteractible

--- npchighlight/src/main/java/net/runelite/client/plugins/npchighlight/NpcSceneOverlay.java
+++ npchighlight/src/main/java/net/runelite/client/plugins/npchighlight/NpcSceneOverlay.java
@@ -137,6 +137,12 @@ public class NpcSceneOverlay extends Overlay
 
 	private void renderNpcOverlay(Graphics2D graphics, NPC actor, Color color)
 	{
+		NPCDefinition npcDefinition = actor.getTransformedDefinition();
+		if (npcDefinition == null || !npcDefinition.isInteractible())
+		{
+			return;
+		}
+
 		if (config.drawInteracting() && actor.getInteracting() != null
 			&& actor.getInteracting() == client.getLocalPlayer())
 		{
