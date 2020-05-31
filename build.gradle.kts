42e81dc434e2a529fa6e7378aff702f80816cf52
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-05-31 06:59:48 +0200

    radiance: Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,7 @@ subprojects {
         compileOnly(group = "com.squareup.okhttp3", name = "okhttp", version = "4.7.2")
         compileOnly(group = "org.pf4j", name = "pf4j", version = "3.3.1")
         compileOnly(group = "io.reactivex.rxjava3", name = "rxjava", version = "3.0.4")
-        compileOnly(group = "org.pushing-pixels", name = "radiance-substance", version = "2.5.1")
+        compileOnly(group = "org.pushing-pixels", name = "radiance-substance", version = "3.0.0")
 
         testAnnotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.12")
 
