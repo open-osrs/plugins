71b6910cfbb5ed250b8328d8adaedba510a10a49
Kyle <48519776+xKylee@users.noreply.github.com>
2020-06-03 16:26:02 +0100

    Update HouseMode.java

--- menuentryswapper/src/main/java/net/runelite/client/plugins/menuentryswapper/util/HouseMode.java
+++ menuentryswapper/src/main/java/net/runelite/client/plugins/menuentryswapper/util/HouseMode.java
@@ -25,11 +25,6 @@
 
 package net.runelite.client.plugins.menuentryswapper.util;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum HouseMode
 {
 	ENTER("Enter"),
