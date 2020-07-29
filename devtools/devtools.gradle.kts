cc1f91236eac34577680a6b7c18041744f40264a
ImNotSatan <kylian105@hotmail.com>
2020-07-29 23:28:22 +0200

    DevTools: Fix Camera Position InfoBox (#373)
    * Fix Camera Poistion InfoPanel
    
    Removed:
    private final PanelComponent panelComponent = new PanelComponent();
    
    * Update devtools.gradle.kts
    
    * Update CameraOverlay.java
    
    Co-authored-by: ImNotSatan <kylian@iphone.nl>

--- devtools/devtools.gradle.kts
+++ devtools/devtools.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.21"
+version = "0.0.22"
 
 project.extra["PluginName"] = "Developer Tools"
 project.extra["PluginDescription"] = "Enables the developer tools"
