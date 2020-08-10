3c0ae8b432b824e0cea9eb02ffee02b361ce0526
Ben Riggs <42555892+ben93riggs@users.noreply.github.com>
2020-08-10 14:59:53 -0500

    Bumped all plugin versions to hopefully resolve hash mismatches

--- autotheiver/autotheiver.gradle.kts
+++ autotheiver/autotheiver.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.3.0"
+version = "0.4.0"
 
 project.extra["PluginName"] = "Auto Theiver"
 project.extra["PluginDescription"] = "Automatically theives from npcs"
