44f3b5ffa26e132815117ff08c3d4581a94b7a2f
emiljensen2 <55587410+emiljensen2@users.noreply.github.com>
2020-08-16 17:55:44 +0200

    Update AntiDragPlugin.java

--- antidrag/src/main/java/net/runelite/client/plugins/antidrag/AntiDragPlugin.java
+++ antidrag/src/main/java/net/runelite/client/plugins/antidrag/AntiDragPlugin.java
@@ -166,7 +166,7 @@ public class AntiDragPlugin extends Plugin
 	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
 	{
-		if (widgetLoaded.getGroupId() == WidgetID.BANK_GROUP_ID || widgetLoaded.getGroupId() == WidgetID.BANK_INVENTORY_GROUP_ID)
+		if (widgetLoaded.getGroupId() == WidgetID.BANK_GROUP_ID || widgetLoaded.getGroupId() == WidgetID.BANK_INVENTORY_GROUP_ID || widgetLoaded.getGroupId() == WidgetID.DEPOSIT_BOX_GROUP_ID)
 		{
 			setBankDragDelay(config.alwaysOn() ? config.bankDragDelay() : DEFAULT_DELAY);
 		}
@@ -250,6 +250,7 @@ public class AntiDragPlugin extends Plugin
 	{
 		final Widget bankItemContainer = client.getWidget(WidgetInfo.BANK_ITEM_CONTAINER);
 		final Widget bankInventoryContainer = client.getWidget(WidgetInfo.BANK_INVENTORY_ITEMS_CONTAINER);
+		final Widget bankDepositContainer = client.getWidget(WidgetInfo.DEPOSIT_BOX_INVENTORY_ITEMS_CONTAINER);
 
 		if (bankItemContainer != null)
 		{
@@ -262,6 +263,14 @@ public class AntiDragPlugin extends Plugin
 		if (bankInventoryContainer != null)
 		{
 			Widget[] items = bankInventoryContainer.getDynamicChildren();
+			for (Widget item : items)
+			{
+				item.setDragDeadTime(delay);
+			}
+		}
+		if (bankDepositContainer != null)
+		{
+			Widget[] items = bankDepositContainer.getDynamicChildren();
 			for (Widget item : items)
 			{
 				item.setDragDeadTime(delay);
