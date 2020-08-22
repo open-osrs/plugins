0cf69c6eceb5a944fa9a45f8f9a485d52a7e58d0
Kyle <48519776+xKylee@users.noreply.github.com>
2020-08-22 23:50:22 +0100

    Update thieving.gradle.kts

--- thieving/thieving.gradle.kts
+++ thieving/thieving.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.6"
+version = "0.0.7"
 
 project.extra["PluginName"] = "Thieving"
 project.extra["PluginDescription"] = "Show thieving overlay"
