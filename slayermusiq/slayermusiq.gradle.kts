510bcfad149a87bde75dc1753de1e98c77279733
Kyle <48519776+xKylee@users.noreply.github.com>
2020-09-18 10:17:58 +0100

    Update slayermusiq.gradle.kts

--- slayermusiq/slayermusiq.gradle.kts
+++ slayermusiq/slayermusiq.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.5"
+version = "0.0.6"
 
 project.extra["PluginName"] = "Slayermusiq1 Guides"
 project.extra["PluginDescription"] = "Adds a right-click option to go to Slayermusiq1's guides from the quest tab"
