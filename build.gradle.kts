ef216e4d89593f8644da4cf150cff10b6a096fe4
OpenOSRS <openosrs.github@gmail.com>
2020-10-21 00:07:50 +0000

    gradle: Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ subprojects {
     apply(plugin = "com.simonharrer.modernizer")
 
     dependencies {
-        annotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.12")
+        annotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.16")
         annotationProcessor(group = "org.pf4j", name = "pf4j", version = "3.4.1")
 
         compileOnly(group = "com.openosrs", name = "http-api", version = ProjectVersions.openosrsVersion)
@@ -75,18 +75,18 @@ subprojects {
         compileOnly(group = "com.openosrs", name = "runelite-client", version = ProjectVersions.openosrsVersion)
 
         compileOnly(group = "org.apache.commons", name = "commons-text", version = "1.9")
-        compileOnly(group = "com.google.guava", name = "guava", version = "29.0-jre")
+        compileOnly(group = "com.google.guava", name = "guava", version = "30.0-jre")
         compileOnly(group = "com.google.inject", name = "guice", version = "4.2.3", classifier = "no_aop")
         compileOnly(group = "com.google.code.gson", name = "gson", version = "2.8.6")
         compileOnly(group = "net.sf.jopt-simple", name = "jopt-simple", version = "5.0.4")
         compileOnly(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
-        compileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.12")
+        compileOnly(group = "org.projectlombok", name = "lombok", version = "1.18.16")
         compileOnly(group = "com.squareup.okhttp3", name = "okhttp", version = "4.9.0")
         compileOnly(group = "org.pf4j", name = "pf4j", version = "3.4.1")
-        compileOnly(group = "io.reactivex.rxjava3", name = "rxjava", version = "3.0.6")
+        compileOnly(group = "io.reactivex.rxjava3", name = "rxjava", version = "3.0.7")
         compileOnly(group = "org.pushing-pixels", name = "radiance-substance", version = "2.5.1")
 
-        testAnnotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.12")
+        testAnnotationProcessor(group = "org.projectlombok", name = "lombok", version = "1.18.16")
 
         testImplementation(group = "com.openosrs", name = "http-api", version = ProjectVersions.openosrsVersion)
         testImplementation(group = "com.openosrs", name = "runelite-api", version = ProjectVersions.openosrsVersion)
@@ -96,10 +96,10 @@ subprojects {
         testImplementation(group = "com.google.inject.extensions", name = "guice-testlib", version = "4.2.3")
         testImplementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
         testImplementation(group = "net.sf.jopt-simple", name = "jopt-simple", version = "5.0.4")
-        testImplementation(group = "junit", name = "junit", version = "4.13")
-        testImplementation(group = "org.mockito", name = "mockito-core", version = "3.5.13")
-        testImplementation(group = "org.mockito", name = "mockito-inline", version = "3.5.13")
-        testImplementation(group = "org.projectlombok", name = "lombok", version = "1.18.12")
+        testImplementation(group = "junit", name = "junit", version = "4.13.1")
+        testImplementation(group = "org.mockito", name = "mockito-core", version = "3.5.15")
+        testImplementation(group = "org.mockito", name = "mockito-inline", version = "3.5.15")
+        testImplementation(group = "org.projectlombok", name = "lombok", version = "1.18.16")
         testImplementation(group = "org.hamcrest", name = "hamcrest-library", version = "2.2")
         testImplementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.30")
     }
