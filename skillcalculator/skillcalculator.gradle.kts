12a57824ceb11365a76ff4b4ad6b024021488092
Az <Ezivoz@gmail.com>
2020-11-14 19:50:18 +0100

    bumped plugin version

--- skillcalculator/skillcalculator.gradle.kts
+++ skillcalculator/skillcalculator.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.1.1"
+version = "0.1.2"
 
 project.extra["PluginName"] = "Skill Calculator"
 project.extra["PluginDescription"] = "Enable the Skill Calculator panel"
