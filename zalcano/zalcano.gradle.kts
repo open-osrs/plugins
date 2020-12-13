6a4652c0971eef48455df291eea20db22f8c57b4
Kyle <48519776+xKylee@users.noreply.github.com>
2020-12-13 22:24:54 +0000

    project: fix zalcano (#536)
    * Update zalcano.gradle.kts
    
    * Update ZalcanoPlugin.java

--- zalcano/zalcano.gradle.kts
+++ zalcano/zalcano.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.1"
+version = "0.0.10"
 
 project.extra["PluginName"] = "Zalcano"
 project.extra["PluginDescription"] = "Assistance for the Zalcano fight"
