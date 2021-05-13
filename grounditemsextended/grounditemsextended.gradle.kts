9da68408b46735332f6931e9580a695cb0f7077b
Kyle <48519776+xKylee@users.noreply.github.com>
2021-05-13 21:48:15 +0100

    Update grounditemsextended.gradle.kts

--- grounditemsextended/grounditemsextended.gradle.kts
+++ grounditemsextended/grounditemsextended.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.openosrsVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.1"
+version = "0.0.2"
 
 project.extra["PluginName"] = "Ground Items Extended"
 project.extra["PluginDescription"] = "Highlight ground items and/or show price information"
