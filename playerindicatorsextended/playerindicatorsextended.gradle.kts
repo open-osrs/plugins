61d51c2b02abd9910a3600b583b5b47e7459cb43
Owain van Brakel <owain.vanbrakel@gmail.com>
2022-04-14 04:53:59 +0200

    project: Bump

--- playerindicatorsextended/playerindicatorsextended.gradle.kts
+++ playerindicatorsextended/playerindicatorsextended.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.openosrsVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.1.1"
+version = "5.1.2"
 
 project.extra["PluginName"] = "Player Indicators Extended"
 project.extra["PluginDescription"] = "Highlight players on-screen and/or on the minimap"
